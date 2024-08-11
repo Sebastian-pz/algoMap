897ee42b0459382afb9586e4b582fb83dc1f657a
Sebastian Perez <spezuluaga@gmail.com>
2024-08-11 15:57:29 -0500

    feat(code): solve problem 11

---
+++ Arrays-and-strings/rotate-image.ts
@@ -0,0 +1,25 @@
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+function rotate(matrix: number[][]): void {
+  const n = matrix.length;
+
+  // Step 1 -> transpose
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // Step 2 -> Horizontal reflection
+  matrix.forEach((row) => row.reverse());
+}
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+rotate(matrix);
+console.log(matrix);
